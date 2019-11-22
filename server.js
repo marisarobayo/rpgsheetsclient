a2185461c23f47cf58428a999ad11d05e79f4afb
Marisa Robayo Moreno <marisarobayo@gmail.com>
2019-11-22 17:07:05 +0100

    Fix forcing HTTPS

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const forceSecure = require("force-secure-express");
 
 app.use(express.static(__dirname + "/dist/"));
 
-app.use(forceSecure([
+/*app.use(forceSecure([
     "rpgsheets.herokuapp.com"
-  ]));
+  ]));*/
+
+app.use(requireHTTPS);
 
 app.get(/.*/, function(req, res) {
     res.sendfile(__dirname + "/dist/index.html");
@@ -15,3 +17,12 @@ app.get(/.*/, function(req, res) {
 app.listen(port);
 
 console.log("Server started");
+
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
