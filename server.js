e6f8e2fb892dfba8c952bd188f17966875f14a8b
Marisa Robayo Moreno <marisarobayo@gmail.com>
2019-11-22 17:15:06 +0100

    Another fix.

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ console.log("Server started");
 
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
+  if (!req.secure || req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.baseUrl);
   }
   next();
 }
